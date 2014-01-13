a21ccdb861ecf4c5f94dd0bec7465494bb74dc84
Patrick D. Kelly <e@pdk.to>
2014-01-13 09:02:50 -1000

    new stolen bicycle data exchange app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sbdx::Application.config.secret_token = '614092919a9b0168b0959f39900df18e9cfb86a5fe9d99d59d07a39f858ed93f7821bfc30dc596949c4b652fdd668b36ff35404e5577e2cb0abad139c67f76c7'
